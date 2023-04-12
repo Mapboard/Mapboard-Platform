b95301d6e60effd3f7dd19fb707f7dc6d00815e3
Daven Quinn <dev@davenquinn.com>
2023-04-12 02:10:24 -0500

    Updated mapboard CLI somewhat

--- mapboard-cli/mapboard/cli/__init__.py
+++ mapboard-cli/mapboard/cli/__init__.py
@@ -6,30 +6,57 @@ import rich
 import sys
 from .compose import compose, check_status, follow_logs
 from typer.core import TyperGroup
+from typer.models import TyperInfo
 from typer import Context
+from typer import Typer
+from dotenv import load_dotenv
+from .definitions import MAPBOARD_ROOT
+
+load_dotenv(MAPBOARD_ROOT / ".env")
 
 
 class OrderCommands(TyperGroup):
     def list_commands(self, ctx: Context):
-        """Return list of commands in the order appear."""
+        """Return list of commands in the order of appearance."""
         return list(self.commands)  # get commands using self.commands
 
 
-app = typer.Typer(no_args_is_help=True, add_completion=False, cls=OrderCommands)
+class ControlCommand(Typer):
+    name: str
+
+    def __init__(self, name, *args, **kwargs):
+        kwargs.setdefault("add_completion", False)
+        kwargs.setdefault("no_args_is_help", True)
+        kwargs.setdefault("cls", OrderCommands)
+        kwargs.setdefault("name", name)
+        super().__init__(*args, **kwargs)
+        self.name = name
+
+        def callback(ctx: Context):
+            ctx.obj = ApplicationSettings(self.name)
+
+        callback.__doc__ = f"""{self.name} command-line interface"""
+
+        self.registered_callback = TyperInfo(callback=callback)
+
+
+app = ControlCommand(name="Mapboard")
 
 console = rich.console.Console()
 
 
-@app.callback()
-def callback():
-    """
-    Mapboard command-line interface
-    """
+class ApplicationSettings:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
 
 
 @app.command()
-def up(container: str = "", force_recreate: bool = False):
+def up(ctx: Context, container: str = "", force_recreate: bool = False):
     """Start the Mapboard server and follow logs."""
+    settings = ctx.find_object(ApplicationSettings)
+
     res = compose(
         "up",
         "-d",
@@ -40,27 +67,28 @@ def up(container: str = "", force_recreate: bool = False):
     )
     if res.returncode != 0:
         console.print(
-            "[red bold]One or more containers did not build successfully, aborting."
+            "One or more containers did not build successfully, aborting.",
+            style="red bold",
         )
         sys.exit(res.returncode)
     else:
-        console.print("[green bold]All containers built successfully.")
+        console.print("All containers built successfully.", style="green bold")
 
-    console.print("[bold]Starting Mapboard server...")
-    check_status("Mapboard", "mapboard")
-    follow_logs("Mapboard", "mapboard")
+    console.print(f"[bold]Starting {settings.name} server...")
+    check_status(settings.name, settings.name.lower())
+    follow_logs(settings.name, settings.name.lower())
 
 
 @app.command()
 def down():
-    """Stop the Mapboard server."""
+    """Stop the server."""
     console.print("[bold]Stopping Mapboard server...")
     compose("down", "--remove-orphans")
 
 
 @app.command()
 def restart(ctx: Context):
-    """Restart the Mapboard server and follow logs."""
+    """Restart the server and follow logs."""
     ctx.invoke(up, force_recreate=True)
 
 
