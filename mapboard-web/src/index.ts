19a2fd0331a28b89c569cec67a6d8bcd812f7ea0
Daven Quinn <dev@davenquinn.com>
2023-04-22 02:23:29 -0500

    Appears to work again

--- mapboard-web/src/index.ts
+++ mapboard-web/src/index.ts
@@ -1,12 +1,21 @@
 import "babel-polyfill";
 import h from "@macrostrat/hyper";
-import { render } from "react-dom";
 import { FocusStyleManager } from "@blueprintjs/core";
+import { createRoot } from "react-dom/client";
+import { Box } from "@macrostrat/ui-components";
+//import { useEffect } from "react";
 
 import { MapApp } from "./app";
 import "@blueprintjs/core/lib/css/blueprint.css";
+import "../web-components/packages/style-system/main.sass";
 import "./main.styl";
+
+// function Inspector() {
+//   return h(Box, "Hello world!");
+// }
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
-render(h(MapApp), document.getElementById("app"));
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(h(MapApp));
--- mapboard-web/vite.config.js
+++ mapboard-web/vite.config.js
@@ -1,26 +1,26 @@
 import * as path from "path";
 import fs from "fs";
 
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
+const pkg = JSON.parse(
+  fs.readFileSync("./web-components/package.json", "utf-8")
+);
 
 // Web components aliases
 let aliases = [];
-for (const [key, value] of Object.entries(pkg.dependencies)) {
-  if (value.startsWith("workspace:")) {
-    const name = key.replace("@macrostrat/", "");
-    const root = path.resolve(
-      __dirname,
-      path.join("web-components", "packages", name, "src")
-    );
-    aliases.push({
-      find: key + "/src",
-      replacement: root,
-    });
-    aliases.push({
-      find: new RegExp(`^${key}$`),
-      replacement: root + "/index.ts",
-    });
-  }
+for (const [key, value] of Object.entries(pkg.alias)) {
+  const name = key.replace("@macrostrat/", "");
+  const root = path.resolve(
+    __dirname,
+    path.join("web-components", "packages", name, "src")
+  );
+  aliases.push({
+    find: key + "/src",
+    replacement: root,
+  });
+  aliases.push({
+    find: new RegExp(`^${key}$`),
+    replacement: root + "/index.ts",
+  });
 }
 
 export default {
