24bbfed3dd8eb0a2df0c7aadd0c75243ec8c6b14
Daven Quinn <code@davenquinn.com>
2024-10-26 01:40:19 -0500

    Updated map page

--- mapboard-ui/pages/project/@slug/map/+Page.client.ts
+++ mapboard-ui/pages/project/@slug/map/+Page.client.ts
@@ -1,7 +1,12 @@
 import { DevMapPage } from "@macrostrat/map-interface";
 import h from "@macrostrat/hyper";
 import { mapboxToken } from "~/settings";
+import { useInDarkMode } from "@macrostrat/ui-components";
 
 export function Page() {
-  return h(DevMapPage, { mapboxToken });
+  const inDarkMode = useInDarkMode();
+  const style = inDarkMode
+    ? "mapbox://styles/jczaplewski/cl5uoqzzq003614o6url9ou9z"
+    : "mapbox://styles/jczaplewski/clatdbkw4002q14lov8zx0bm0";
+  return h(DevMapPage, { mapboxToken, style });
 }
