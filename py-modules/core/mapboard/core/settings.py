f0f2834c9004febf2d5ca046be239a9938acc947
Daven Quinn <code@davenquinn.com>
2024-10-27 13:34:12 -0500

    Updated topology watching

--- py-modules/core/mapboard/core/settings.py
+++ py-modules/core/mapboard/core/settings.py
@@ -4,32 +4,29 @@ from pathlib import Path
 from dotenv import load_dotenv
 from macrostrat.database import Database
 
-MAPBOARD_CLI_ROOT = Path(__file__).parent
-MAPBOARD_ROOT = MAPBOARD_CLI_ROOT.parent.parent.parent
-
-
 # For some reason, environment variables aren't loading correctly
 # using the app_frame module. Or maybe, env vars set there
 # aren't available outside of module code.
-load_dotenv(MAPBOARD_ROOT / ".env")
-# Could probably manage this within the application config.
+MAPBOARD_ROOT = environ.get("MAPBOARD_ROOT", None)
+if MAPBOARD_ROOT is not None:
+    MAPBOARD_ROOT = Path(MAPBOARD_ROOT)
+    load_dotenv(MAPBOARD_ROOT / ".env")
 
 
 def connection_string(database: str, container_internal: bool = False):
     """Get a connection string for a given database"""
-    POSTGRES_USER = environ.get("POSTGRES_USER") or "postgres"
-    POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD") or "postgres"
+    user = environ.get("POSTGRES_USER") or "postgres"
+    password = environ.get("POSTGRES_PASSWORD") or "postgres"
     if container_internal:
-        PORT = 5432
-        HOST = "database"
+        port = 5432
+        host = "database"
     else:
-        PORT = environ.get("MAPBOARD_DB_PORT") or 54398
-        HOST = environ.get("POSTGRES_HOST") or "localhost"
+        port = environ.get("MAPBOARD_DB_PORT") or 54398
+        host = environ.get("POSTGRES_HOST") or "localhost"
 
-    return f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{HOST}:{PORT}/{database}"
+    return f"postgresql://{user}:{password}@{host}:{port}/{database}"
 
 
 POSTGRES_IMAGE = environ.get("POSTGRES_IMAGE") or "postgis/postgis:13-3.1"
-
 
 core_db = Database(connection_string("mapboard"))
--- py-modules/core/mapboard/core/workers/topology.py
+++ py-modules/core/mapboard/core/workers/topology.py
@@ -1,22 +1,25 @@
+"""
+Worker that continuously checks for new topology tasks and
+sends them to the broker for processing.
+"""
 import asyncio
 from contextvars import ContextVar
+from datetime import datetime
 from json import loads
 
-from macrostrat.app_frame.compose import console
 from mapboard.topology_manager.commands.update import _update
 from mapboard.topology_manager.database import Database
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from datetime import datetime
 
-from .database import core_db
+from mapboard.core.settings import core_db, connection_string
+from json import dumps
 
 verbose = True
-
 
 update_in_progress = ContextVar("update_in_progress", default=False)
 needs_update = ContextVar("needs_update", default=set())
 clients = ContextVar("clients", default={})
-
-from .database import connection_string
 
 
 def project_params(data_schema: str):
@@ -52,8 +55,8 @@ def get_client(database: str, data_schema: str) -> Database:
     return db
 
 
-def watch(database: str):
-    """Watch a database for topology changes.
+def watch_topology(database: str):
+    """Watch database(s) for topology changes.
 
     Unlike the core watcher in
     mapboard.topology_manager, this works across multiple projects,
@@ -62,30 +65,8 @@ def watch(database: str):
     DATABASE_URL = connection_string(database)
     main_db = Database(DATABASE_URL)
 
-    def _update_topology(database):
-        status = needs_update.get()
-
-        if len(status) == 0:
-            return
-        if update_in_progress.get():
-            return
-
-        print("Updating topology", status)
-
-        update_in_progress.set(True)
-        next_schema = status.pop()
-        needs_update.set(status)
-
-        print(f"Updating topology for {next_schema}")
-        # Do the update
-        db = get_client(database, next_schema)
-        print(f"Updating topology for {next_schema}", db)
-        _update(db)
-        update_in_progress.set(False)
-
-    conn = main_db.engine.connect()
     # Get a raw connection to listen for notifications
-    conn = conn.connection
+    conn = _raw_connection(main_db)
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
     cursor = conn.cursor()
@@ -95,20 +76,76 @@ def watch(database: str):
         conn.poll()
         for notify in conn.notifies:
             json_payload = loads(notify.payload)
-            schema = json_payload.get("schema")
 
-            print(json_payload)
+            type = json_payload.get("type", None)
+            if type == "test":
+                print("Received test event", json_payload)
+                continue
+
+            schema = json_payload.get("schema")
 
             status = needs_update.get()
             status.add(schema)
             needs_update.set(status)
             # We can do this async eventually, hopefully.
-            _update_topology(database)
-            if len(needs_update.get()) > 0:
-                _update_topology(database)
+            # _update_topology(database)
+            # if len(needs_update.get()) > 0:
+            #     _update_topology(database)
         conn.notifies.clear()
 
-    console.print(f"Watching database {database} for topology changes...")
+    print(f"Watching database {database} for topology changes...")
     loop = asyncio.get_event_loop()
+
     loop.add_reader(conn, handle_notify)
+    loop.create_task(update_topology_sequentially(database))
+
     loop.run_forever()
+
+
+async def update_topology_sequentially(database: str):
+    """Check for topology updates and run them sequentially"""
+    while True:
+        await _update_topology(database)
+
+
+def _raw_connection(database: Database):
+    conn = database.engine.connect()
+    return conn.connection
+
+
+async def _update_topology(database):
+    status = needs_update.get()
+
+    if len(status) == 0 or update_in_progress.get():
+        await asyncio.sleep(1)
+        return
+
+    print("Updating topology", status)
+
+    update_in_progress.set(True)
+    next_schema = status.pop()
+    needs_update.set(status)
+
+    print(f"Updating topology for {next_schema}")
+    # Do the update
+    db = get_client(database, next_schema)
+    print(f"Updating topology for {next_schema}", db)
+    _update(db)
+    update_in_progress.set(False)
+
+
+def send_event(database: str):
+    """Send an event to the database for testing purposes"""
+    url = connection_string(database)
+    db = Database(url)
+    conn = _raw_connection(db)
+    cursor = conn.cursor()
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    test_event = dict(
+        type="test",
+        time=datetime.now().isoformat(),
+    )
+    test_event = dumps(test_event)
+
+    cursor.execute("NOTIFY events, %s;", (test_event,))
+    print(f"Sent {test_event} to {database}")
