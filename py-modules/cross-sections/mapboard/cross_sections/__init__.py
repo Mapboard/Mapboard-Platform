f5d78f64926899eebbb6ab7f72879c76a17f65f8
Daven Quinn <code@davenquinn.com>
2024-10-28 06:14:04 -0500

    Updated cross section design

--- py-modules/cross-sections/mapboard/cross_sections/__init__.py
+++ py-modules/cross-sections/mapboard/cross_sections/__init__.py
@@ -6,9 +6,25 @@ from mapboard.core.database import setup_database
 app = Typer(name="cross-sections", no_args_is_help=True)
 
 fixtures = Path(__file__).parent / "fixtures"
+procedures = Path(__file__).parent / "procedures"
 
 
-@app.command(name="cross-sections")
+@app.command(name="update")
 def cross_sections():
-    db = setup_database("naukluft_cross_sections")
+    """
+    Create fixtures for the cross-sections
+    """
+    db = setup_database("naukluft-cross-sections")
     db.run_fixtures(fixtures)
+
+
+@app.command(name="get-terrain")
+def get_terrain():
+    """
+    Update the topography of the cross-sections
+    """
+
+    db = setup_database("naukluft-cross-sections")
+
+    proc = procedures / "update-topography.sql"
+    db.run_sql(proc)
