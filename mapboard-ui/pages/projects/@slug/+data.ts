ab23642752ed055613bd4f0af90a2cc5df2c0eb5
Daven Quinn <code@davenquinn.com>
2024-11-06 04:50:07 -0600

    Updated project page

--- mapboard-ui/pages/projects/@slug/+data.ts
+++ mapboard-ui/pages/projects/@slug/+data.ts
@@ -5,15 +5,28 @@ import type { definitions } from "~/types/project-api";
 
 export type Data = Awaited<ReturnType<typeof data>>;
 
+type ExtProject = definitions["project"] & {
+  contexts: definitions["context"][];
+};
+
 export const data = async (pageContext: PageContextServer) => {
   // https://vike.dev/useConfig
   const config = useConfig();
 
-  const response = await postgrest
+  const projRequest = postgrest
     .from("project")
     .select()
     .eq("slug", pageContext.routeParams.slug);
-  let project: definitions["project"] = response.data?.[0];
+
+  const ctxRequest = postgrest
+    .from("context")
+    .select("id,slug,name,type,is_main_context")
+    .eq("project_slug", pageContext.routeParams.slug);
+
+  const [projects, contexts] = await Promise.all([projRequest, ctxRequest]);
+
+  let project: ExtProject = projects.data?.[0];
+  project.contexts = contexts.data as definitions["context"][];
 
   config({
     // Set <title>
