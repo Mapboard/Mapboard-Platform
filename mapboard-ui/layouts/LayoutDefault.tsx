0bb3cb8d0a5627ef4d566af4d10f79f6f0b9262f
Daven Quinn <code@davenquinn.com>
2025-02-19 18:23:51 -0600

    Basic layout works

--- mapboard-ui/layouts/LayoutDefault.tsx
+++ mapboard-ui/layouts/LayoutDefault.tsx
@@ -1,74 +1,59 @@
 import "./style.css";
 
 import React from "react";
-import logoUrl from "../assets/logo.svg";
 import { Link } from "~/components/link";
+import h from "@macrostrat/hyper";
 
 export default function LayoutDefault({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        maxWidth: 900,
-        margin: "auto"
-      }}
-    >
-      <Sidebar>
-        <Logo />
-        <Link href="/">Welcome</Link>
-        {""}
-      </Sidebar>
-      <Content>{children}</Content>
-    </div>
+  return h(
+    "div",
+    { style: { display: "flex", maxWidth: 900, margin: "auto" } },
+    [
+      h(Sidebar, [
+        h(Logo),
+        h(Link, { href: "/" }, "Welcome"),
+        h(Link, { href: "/docs" }, "Documentation")
+      ]),
+      h(Content, children)
+    ]
   );
 }
 
 function Sidebar({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      id="sidebar"
-      style={{
+  return h(
+    "div#sidebar",
+    {
+      style: {
         padding: 20,
         flexShrink: 0,
         display: "flex",
         flexDirection: "column",
-        lineHeight: "1.8em",
-        borderRight: "2px solid #eee"
-      }}
-    >
-      {children}
-    </div>
+        lineHeight: "1.8em"
+      }
+    },
+    children
   );
 }
 
 function Content({ children }: { children: React.ReactNode }) {
-  return (
-    <div id="page-container">
-      <div
-        id="page-content"
-        style={{
-          padding: 20,
-          paddingBottom: 50,
-          minHeight: "100vh"
-        }}
-      >
-        {children}
-      </div>
-    </div>
-  );
+  return h("div#page-container", [
+    h(
+      "div#page-content",
+      { style: { padding: 20, paddingBottom: 50, minHeight: "100vh" } },
+      [children]
+    )
+  ]);
 }
 
 function Logo() {
-  return (
-    <div
-      style={{
-        marginTop: 20,
-        marginBottom: 10
-      }}
-    >
-      <a href="/">
-        <img src={logoUrl} height={64} width={64} alt="logo" />
-      </a>
-    </div>
-  );
+  return h("div.logo", { style: { marginBottom: 10, marginTop: 20 } }, [
+    h("a", { href: "/" }, [
+      h("img", {
+        src: "https://mapboard-gis.app/img/mapboard-icon.png",
+        height: 100,
+        width: 100,
+        alt: "logo"
+      })
+    ])
+  ]);
 }
