aa4c4d85bd1a877713b77176fa44b9cfd06913c0
Daven Quinn <code@davenquinn.com>
2025-05-06 01:26:24 -0500

    Updated database management

--- py-modules/cli/mapboard/cli/database.py
+++ py-modules/cli/mapboard/cli/database.py
@@ -5,10 +5,11 @@ from subprocess import run
 from sys import stdin
 from typing import Optional
 from rich.console import Console
+from datetime import datetime
 
 from macrostrat.app_frame.compose import console
-from macrostrat.database import run_sql, run_query
-from macrostrat.database.transfer.utils import raw_database_url
+from macrostrat.database import run_sql
+from macrostrat.database.transfer import pg_dump_to_file
 from macrostrat.dinosaur import create_migration
 from macrostrat.utils.shell import run
 from mapboard.topology_manager.database import Database
@@ -18,6 +19,7 @@ from typer import Argument, Context, Option, Typer
 from .fixtures import apply_core_fixtures, apply_fixtures, create_core_fixtures
 from mapboard.core.settings import connection_string, core_db
 from mapboard.core.database import project_params, setup_database
+import asyncio
 
 db_app = Typer(name="db", no_args_is_help=True)
 
@@ -149,3 +151,25 @@ def disconnect(project: Optional[str] = None):
 
         """
     )
+
+
+@db_app.command()
+def dump(
+    project: str,
+    dumpfile: Optional[Path] = None
+):
+    """Dump a Mapboard project database to a file"""
+    if project == "mapboard" or project == "-":
+        project = "mapboard"
+        db = core_db
+    else:
+        params = project_params(project)
+        database = params.pop("database")
+        db = Database(connection_string(database))
+
+    if dumpfile is None:
+        date_string = datetime.now().strftime("%Y-%m-%d")
+        dumpfile = Path(f"{project}-{date_string}.pg-dump")
+
+    task = pg_dump_to_file(db.engine, dumpfile)
+    asyncio.run(task)
