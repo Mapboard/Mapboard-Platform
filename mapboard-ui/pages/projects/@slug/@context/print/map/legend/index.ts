d95558df40345ce503d4b61dec1b9e9648b3cdc1
Daven Quinn <code@davenquinn.com>
2025-10-11 05:44:20 -0500

    Updated composite units

--- mapboard-ui/pages/projects/@slug/@context/print/map/legend/index.ts
+++ mapboard-ui/pages/projects/@slug/@context/print/map/legend/index.ts
@@ -2,6 +2,7 @@ import hyper from "@macrostrat/hyper";
 import style from "./legend.module.sass";
 import { postgrest } from "~/utils/api-client";
 import { atom, useAtomValue } from "jotai";
+import classNames from "classnames";
 
 const h = hyper.styled(style);
 
@@ -22,6 +23,85 @@ export function LegendPanel() {
   ]);
 }
 
+function MapLegendList() {
+  const undiv = "Undivided";
+
+  return h("div.map-units-list", [
+    g("Cover", [u("dune"), u("alluvium"), u("colluvium"), u("tufa")]),
+    g("Nama Group", [
+      u("urusis"),
+      g("Zaris Formation", [
+        u("urikos", "Urikos Member"),
+        u("houghland", "Hoogland Member"),
+        g("Omkyk Member", [
+          h(CompositeUnit, {
+            subsidiary: { id: "upper-omkyk-grainstone", name: "Biostrome" },
+            main: { id: "upper-omkyk", name: "Upper" },
+            relationship: CompositeRelationship.OVERLIES,
+          }),
+          h(CompositeUnit, {
+            subsidiary: { id: "middle-omkyk-reef", name: "Patch reef" },
+            main: { id: "middle-omkyk", name: "Middle" },
+            relationship: CompositeRelationship.PARTIALLY_OVERLIES,
+          }),
+          u("lower-omkyk", "Lower"),
+        ]),
+        u("dabis", "Dabis Member"),
+      ]),
+    ]),
+    g("Naukluft Nappe Complex", [
+      g("Zebra Series", [
+        g("Tafel Formation", [
+          u("adler", "Upper"),
+          u("zebra-limestone", "Lower"),
+        ]),
+        g("Onis Formation", [
+          u("upper-onis", "Upper"),
+          u("middle-onis", "Middle"),
+          u("lower-onis", "Lower"),
+          //u("onis", undiv),
+        ]),
+        g("Lemoenputs Formation", [
+          u("upper-lemoenputs", "Upper"),
+          h(CompositeUnit, {
+            subsidiary: { id: "lemoenputs-ooid", name: "Bed B" },
+            main: { id: "middle-lemoenputs", name: "Middle" },
+            relationship: CompositeRelationship.OVERLIES,
+          }),
+          h(CompositeUnit, {
+            subsidiary: { id: "lemoenputs-a", name: "Bed A" },
+            main: { id: "lower-lemoenputs", name: "Lower" },
+            relationship: CompositeRelationship.OVERLIES,
+          }),
+        ]),
+        g("Tsams Formation", [
+          u("tsams-c", "Upper"),
+          u("tsams-b", "Middle"),
+          u("tsams-a", "Lower"),
+        ]),
+        u("ubisis", "Ubisis Formation"),
+        u("neuras", "Neuras Formation"),
+      ]),
+      g("Dassie Series", [
+        u("dassie", "Büllsport Formation"),
+        u("aubslucht", "Aubslucht Formation"),
+      ]),
+      g("Kudu Series", [
+        u("kudu", "Noab Formation"),
+        u("southern-pavian", "Remhoogte Formation"),
+      ]),
+      g("Pavian Series", [
+        g("Southern Pavian Series", [
+          u("bullsport-outlier", "Büllsport outlier"),
+          u("arbeit-adelt-outlier", "Arbeit Adelt outlier"),
+        ]),
+        u("northern-pavian", "Northern Pavian Series"),
+      ]),
+    ]),
+    g("Pre-Damara basement", [u("basement", "Igneous and metamorphic rocks")]),
+  ]);
+}
+
 async function fetchUnitData() {
   const unitRequest = postgrest
     .from("polygon_type")
@@ -37,8 +117,9 @@ async function fetchUnitData() {
 
 const legendDataAtom = atom(fetchUnitData);
 
-function Swatch({ color, symbol, symbol_color }: any) {
-  let background = color;
+function Swatch({ color, symbol, symbol_color, className }: any) {
+  let backgroundColor = undefined;
+  let backgroundImage = undefined;
 
   if (symbol != null) {
     const params = new URLSearchParams();
@@ -49,11 +130,14 @@ function Swatch({ color, symbol, symbol_color }: any) {
     params.set("background-color", color);
     params.set("scale", "4");
     let url = `/styles/pattern/${symbol}.png?${params.toString()}`;
-    background = `url("${url}")`;
+    backgroundImage = `url("${url}")`;
+  } else {
+    backgroundColor = color;
   }
 
   return h("div.swatch", {
-    style: { background, backgroundSize: "100px" },
+    className,
+    style: { backgroundColor, backgroundImage },
   });
 }
 
@@ -73,19 +157,11 @@ function MapUnit({
 }) {
   const swatchData = useSwatchData(id);
 
-  console.log(swatchData);
+  const unitName = name ?? swatchData?.name;
 
-  if (swatchData == null) {
-    return null;
-  }
-
-  const { name: nameData } = swatchData;
-  if (name == null) {
-    name = nameData;
-  }
   return h("div.unit", [
     h(Swatch, { ...swatchData, unitID: id }),
-    h("div.right", [h("div.label", name)]),
+    h("div.right", [h("div.unit-name", unitName)]),
   ]);
 }
 
@@ -94,76 +170,34 @@ function u(id: string, name?: string, desc?: string) {
 }
 
 function Group({ name, children }) {
-  return h("div.unit-group", [h("div.group-title", name), children]);
+  return h("div.unit-group", [h("div.group-name", name), children]);
 }
 
 function g(name: string, children: any | undefined) {
   return h(Group, { name }, children);
 }
 
-function MapLegendList() {
-  const undiv = "Undivided";
+enum CompositeRelationship {
+  OVERLIES = "overlies",
+  PARTIALLY_OVERLIES = "partially-overlies",
+}
 
-  return h("div#map-units-list", [
-    g("Cover", [u("alluvium"), u("colluvium"), u("tufa"), u("dune")]),
-    g("Nama Group", [
-      u("urusis"),
-      g("Zaris Formation", [
-        u("urikos"),
-        u("houghland", "Hoogland Member"),
-        g("Omkyk Member", [
-          u("upper-omkyk-grainstone", "Biostrome (to upper)"),
-          u("upper-omkyk", "Upper"),
-          u("middle-omkyk"),
-          u("middle-omkyk-reef", "Patch reef (to middle)"),
-          u("lower-omkyk"),
-        ]),
-        u("dabis", "Dabis Member"),
-      ]),
+function CompositeUnit({ main, subsidiary, relationship }) {
+  // Swatch that aligns two patterns with an optional relationship type expressed as a SVG path
+
+  const mainData = useSwatchData(main.id);
+  const subsidiaryData = useSwatchData(subsidiary.id);
+  if (mainData == null || subsidiaryData == null) return null;
+
+  return h("div.unit.composite-unit", [
+    h("div.composite-swatch", { className: relationship }, [
+      h(Swatch, {
+        ...subsidiaryData,
+        className: "subsidiary-swatch",
+      }),
+      h(Swatch, { ...mainData, className: "main-swatch" }),
     ]),
-    g("Naukluft Nappe Complex", [
-      g("Zebra Series", [
-        g("Tafel Formation", [
-          u("adler", "Upper"),
-          u("zebra-limestone", "Lower"),
-        ]),
-        g("Onis Formation", [
-          u("upper-onis", "Upper"),
-          u("middle-onis", "Middle"),
-          u("lower-onis", "Lower"),
-          u("onis", undiv),
-        ]),
-        g("Lemoenputs Formation", [
-          u("upper-lemoenputs", "Upper"),
-          u("lemoenputs-ooid", "Bed B (to middle)"),
-          u("middle-lemoenputs", "Middle"),
-          u("lemoenputs-a", "Bed A (to lower)"),
-          u("lower-lemoenputs", "Lower"),
-        ]),
-        g("Tsams Formation", [
-          u("tsams-c", "Upper"),
-          u("tsams-b", "Middle"),
-          u("tsams-a", "Lower"),
-        ]),
-        u("ubisis", "Ubisis Formation"),
-        u("neuras", "Neuras Formation"),
-      ]),
-      g("Dassie Series", [
-        u("dassie", undiv),
-        u("aubslucht", "Aubslucht Formation"),
-      ]),
-      g("Kudu Series", [
-        u("kudu", undiv),
-        u("southern-pavian", "Shale component"),
-      ]),
-      g("Pavian Series", [
-        g("Southern Pavian Series", [
-          u("bullsport-outlier", "Büllsport outlier"),
-          u("arbeit-adelt-outlier", "Arbeit Adelt outlier"),
-        ]),
-        u("northern-pavian", "Northern Pavian Series"),
-      ]),
-    ]),
-    g("Pre-Damara basement", [u("basement", "Igneous and metamorphic rocks")]),
+    h("div.unit-name.subsidiary", subsidiary.name ?? subsidiaryData.name),
+    h("div.unit-name", main.name ?? mainData.name),
   ]);
 }
